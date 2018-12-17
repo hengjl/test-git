932370d09fac2013320c02836c4924bb0c7a0382
hengjl <614509768@qq.com>
2018-12-17 22:28:37 +0800

    "This is first idea update commit"

--- src/main/java/com/baizhi/testgit/entity/Test.java
+++ src/main/java/com/baizhi/testgit/entity/Test.java
@@ -3,6 +3,7 @@ package com.baizhi.testgit.entity;
 public class Test {
     public static void main(String[] args) {
         System.out.printf("gagag");
+        System.out.printf("gllll");
     }
 
 }
